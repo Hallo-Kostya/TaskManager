0292b35d5b4160ca9e5dca9c82c61251228510a8
Necessless <Necessless@DESKTOP-DF5DPU5>
2024-03-31 21:08:52 +0500

    Добавьте файлы проекта.

---
+++ App1/Views/AboutPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Rg.Plugins.Popup.Contracts;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        private IPopupNavigation _popup { get; set; }
+        private Page1 _modalPage;
+        public AboutPage()
+        {
+            InitializeComponent();
+            _popup = PopupNavigation.Instance;
+            _modalPage = new Page1();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _popup.Popped += Popup_Popped;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _popup.Popped -= Popup_Popped;
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await _popup.PushAsync(_modalPage);
+        }
+        private async void Popup_Popped(object sender, Rg.Plugins.Popup.Events.PopupNavigationEventArgs e)
+        {
+            /* add your logic here, if necessary  */
+        }
+    }
+}
---
+++ App1/Views/Page1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : Rg.Plugins.Popup.Pages.PopupPage
+    {
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+        public void OnAnimationStarted(bool isPopAnimation)
+        {
+            // optional code here   
+        }
+
+
+        public void OnAnimationFinished(bool isPopAnimation)
+        {
+            // optional code here   
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // Return true if you don't want to close this popup page when a back button is pressed
+            return true;
+        }
+
+
+        // Invoked when background is clicked
+        protected override bool OnBackgroundClicked()
+        {
+            // Return false if you don't want to close this popup page when a background of the popup page is clicked
+            return false;
+        }
+
+        private async void CloseBtn_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync(true);
+        }
+    }
+}
