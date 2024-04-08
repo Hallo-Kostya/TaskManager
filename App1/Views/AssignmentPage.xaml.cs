fe1ec1a38a616853ec26aa25d0ddddfe73852f46
Айтур Максутов <aitur1311@gmail.com>
2024-04-08 13:48:32 +0500

    123

--- App1/Views/AssignmentPage.xaml.cs
+++ App1/Views/AssignmentPage.xaml.cs
@@ -14,16 +14,32 @@ namespace App1.Views
     public partial class AssignmentPage : ContentPage
     {
         AssignmentViewModel assignmentViewModel;
+        Button previousButton;
         public AssignmentPage()
         {
             InitializeComponent();
             BindingContext = assignmentViewModel = new AssignmentViewModel(Navigation);
+            DayWeek1.BackgroundColor = Color.FromHex("#952EAF");
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
             assignmentViewModel.OnAppearing();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            if (previousButton != null && previousButton != DayWeek1)
+                previousButton.BackgroundColor = Color.Black;
+
+            clickedButton.BackgroundColor = Color.FromHex("#952EAF");
+
+            previousButton = clickedButton;
+
+            if (clickedButton != DayWeek1)
+                DayWeek1.BackgroundColor = Color.Black;
         }
     }
 }
