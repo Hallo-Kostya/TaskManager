8f710407cf5af983d185fbed20f0d6cc4a0ec96d
DaddyMilkere <Necessless@DESKTOP-DF5DPU5>
2024-04-17 00:14:01 +0500

    fixed editing and deleting tasks

--- App1/ViewModels/AssignmentViewModel.cs
+++ App1/ViewModels/AssignmentViewModel.cs
@@ -1,8 +1,10 @@
-﻿using App1.Models;
+﻿using App1.Data;
+using App1.Models;
 using App1.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,8 @@ namespace App1.ViewModels
 
         public Command EditAssignmentCommand { get; }
         public Command DeleteAssignmentCommand { get; }
+        public ILookup<string, Assignment> GroupByIsCompleted { get; set; }
+        public Command ChangeIsCompletedCommand { get; set; }
         public INavigation Navigation { get; set; }
         public AssignmentViewModel(INavigation _navigation)
         {
@@ -28,11 +32,18 @@ namespace App1.ViewModels
             EditAssignmentCommand = new Command<Assignment>(OnEditAssignment);
             Navigation = _navigation;
             DeleteAssignmentCommand = new Command<Assignment>(OnDeleteAssignment);
+            ChangeIsCompletedCommand = new Command<Assignment>(HandleChangeIsCompleted);
+            GroupByIsCompleted = GetGroupByIsCompleted();
         }
-
+        
         public  void OnAppearing()
         {
             IsBusy = true;
+        }
+        private ILookup<string, Assignment> GetGroupByIsCompleted()
+        {
+            return assignments.OrderBy(t => t.IsCompleted)
+                            .ToLookup(t => t.IsCompleted ? "Completed" : "Active");
         }
         async Task ExecuteLoadAssignmentCommand()
         {
@@ -53,7 +64,12 @@ namespace App1.ViewModels
                 IsBusy = false;
             }
         }
-        
+        private async void HandleChangeIsCompleted(Assignment assignment)
+        {
+            assignment.IsCompleted = !assignment.IsCompleted;
+            await App.AssignmentsDB.AddAssignmentAsync(assignment);
+            await ExecuteLoadAssignmentCommand();
+        }
         private async void OnAddAssignment(object obj)
         {
             await Shell.Current.GoToAsync(nameof(AssignmentAddingPage));
