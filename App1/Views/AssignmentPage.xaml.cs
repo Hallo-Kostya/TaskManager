85a199b6062c8af2f172cfd690719b6509662ca6
Айтур Максутов <aitur1311@gmail.com>
2024-04-22 16:24:30 +0500

    ц

--- App1/Views/AssignmentPage.xaml.cs
+++ App1/Views/AssignmentPage.xaml.cs
@@ -1,8 +1,7 @@
-﻿using App1.Models;
-using App1.Services;
-using App1.ViewModels;
+﻿using App1.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +16,33 @@ namespace App1.Views
     {
         AssignmentViewModel assignmentViewModel;
         Button previousButton;
-        public DateTime tempDate;
         public AssignmentPage()
         {
             InitializeComponent();
             BindingContext = assignmentViewModel = new AssignmentViewModel(Navigation);
-            DayWeek1.BackgroundColor = Color.FromHex("#952EAF");
+            Day1.BackgroundColor = Color.FromHex("#952EAF");
+            DateTime currentDate = DateTime.Now;
+
+            labelMonth.Text = char.ToUpper(currentDate.ToString("MMMM", CultureInfo.CreateSpecificCulture("ru-RU"))[0]) + currentDate.ToString("MMMM", CultureInfo.CreateSpecificCulture("ru-RU")).Substring(1);
+            SetDayOfWeekText(DayWeek1, currentDate);
+            SetDayOfWeekText(DayWeek2, currentDate.AddDays(1));
+            SetDayOfWeekText(DayWeek3, currentDate.AddDays(2));
+            SetDayOfWeekText(DayWeek4, currentDate.AddDays(3));
+            SetDayOfWeekText(DayWeek5, currentDate.AddDays(4));
+            SetDayOfWeekText(DayWeek6, currentDate.AddDays(5));
+            SetDayOfWeekText(DayWeek7, currentDate.AddDays(6));
+            Day1.Text = currentDate.ToString("dd");
+            Day2.Text = currentDate.AddDays(1).ToString("dd");
+            Day3.Text = currentDate.AddDays(2).ToString("dd");
+            Day4.Text = currentDate.AddDays(3).ToString("dd");
+            Day5.Text = currentDate.AddDays(4).ToString("dd");
+            Day6.Text = currentDate.AddDays(5).ToString("dd");
+            Day7.Text = currentDate.AddDays(6).ToString("dd");
+        }
+
+        private void SetDayOfWeekText(Label label, DateTime date)
+        {
+            label.Text = char.ToUpper(date.ToString("ddd", CultureInfo.CreateSpecificCulture("ru-RU"))[0]) + date.ToString("ddd", CultureInfo.CreateSpecificCulture("ru-RU")).Substring(1);
         }
 
         protected override void OnAppearing()
@@ -30,21 +50,19 @@ namespace App1.Views
             base.OnAppearing();
             assignmentViewModel.OnAppearing();
         }
-        private async void Button_Clicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            tempDate=DateTime.Now; ///заглушка
             Button clickedButton = (Button)sender;
 
-            if (previousButton != null && previousButton != DayWeek1)
+            if (previousButton != null && previousButton != Day1)
                 previousButton.BackgroundColor = Color.Black;
 
             clickedButton.BackgroundColor = Color.FromHex("#952EAF");
 
             previousButton = clickedButton;
 
-            if (clickedButton != DayWeek1)
-                DayWeek1.BackgroundColor = Color.Black;
-            await Navigation.PushAsync(new AssignmentPage());
+            if (clickedButton != Day1)
+                Day1.BackgroundColor = Color.Black;
         }
 
         private void AddTask_Clicked(object sender, EventArgs e)
