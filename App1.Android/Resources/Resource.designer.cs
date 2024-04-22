208f9d33edb8678fd954c0a7e14ad858f4d4af41
Айтур Максутов <aitur1311@gmail.com>
2024-04-23 01:37:31 +0500

    x

--- App1.Android/Resources/Resource.designer.cs
+++ App1.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace App1.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.1.111")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.2.120")]
 	public partial class Resource
 	{
 		
@@ -23722,31 +23722,34 @@ namespace App1.Droid
 			public const int preference_list_divider_material = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int settings = 2131165341;
+			public const int search = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int test_custom_background = 2131165342;
+			public const int settings = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int time = 2131165343;
+			public const int test_custom_background = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tooltip_frame_dark = 2131165344;
+			public const int time = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int tooltip_frame_light = 2131165345;
+			public const int tooltip_frame_dark = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int trash = 2131165346;
+			public const int tooltip_frame_light = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int user = 2131165347;
+			public const int trash = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int users = 2131165348;
+			public const int user = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int xamarin_logo = 2131165349;
+			public const int users = 2131165349;
+			
+			// aapt resource value: 0x7F0700A6
+			public const int xamarin_logo = 2131165350;
 			
 			static Drawable()
 			{
