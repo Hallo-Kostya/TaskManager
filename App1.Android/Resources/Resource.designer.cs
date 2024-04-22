9fb7b1680f83b5a7449c4fe1a4ce6c66864712ce
Айтур Максутов <aitur1311@gmail.com>
2024-04-22 21:59:04 +0500

    qw

--- App1.Android/Resources/Resource.designer.cs
+++ App1.Android/Resources/Resource.designer.cs
@@ -23728,22 +23728,25 @@ namespace App1.Droid
 			public const int test_custom_background = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_dark = 2131165343;
+			public const int time = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tooltip_frame_light = 2131165344;
+			public const int tooltip_frame_dark = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int trash = 2131165345;
+			public const int tooltip_frame_light = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int user = 2131165346;
+			public const int trash = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int users = 2131165347;
+			public const int user = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int xamarin_logo = 2131165348;
+			public const int users = 2131165348;
+			
+			// aapt resource value: 0x7F0700A5
+			public const int xamarin_logo = 2131165349;
 			
 			static Drawable()
 			{
