56467d88f8108feb05a23613220a3e0876acac1c
DaddyMilkere <Necessless@DESKTOP-DF5DPU5>
2024-04-23 20:44:39 +0500

    Added Archive page

---
+++ App1/ViewModels/ArchiveViewModel.cs
@@ -0,0 +1,72 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    class ArchiveViewModel:BaseAssignmentViewModel
+    {
+        public Command ClearArchiveCommand { get; }
+        public Command LoadArchiveCommand { get; }
+        public Command DeleteArchivedAssignmentCommand { get; }
+        public Command RecoverAssignmentCommand { get; }
+        public ObservableCollection<AssignmentModel> Archive { get; set; }
+        public ArchiveViewModel()
+        {
+            DeleteArchivedAssignmentCommand = new Command<AssignmentModel>(DeleteAssignment);
+            RecoverAssignmentCommand = new Command<AssignmentModel>(RecoverAssignment);
+            ClearArchiveCommand = new Command(ClearArchive);
+            LoadArchiveCommand = new Command(async () => await ExecuteLoadArchive());
+            Archive = new ObservableCollection<AssignmentModel>();
+        }
+
+        async Task ExecuteLoadArchive()
+        {
+            try
+            {
+                Archive.Clear();
+                var assList = (await App.AssignmentsDB.GetItemsAsync()).Where(t => t.IsDeleted == true);///GetSortedByDate(DateTime date);
+                foreach (var ass in assList)
+                {
+                    Archive.Add(ass);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private async void DeleteAssignment(AssignmentModel assignment)
+        {
+            if (assignment == null)
+            {
+                return;
+            }
+            await App.AssignmentsDB.DeleteItemAsync(assignment.ID);
+            await ExecuteLoadArchive();
+        }
+        private async void RecoverAssignment(AssignmentModel assignment)
+        {
+            if (assignment == null)
+            {
+                return;
+            }
+            assignment.IsDeleted = false;
+            await App.AssignmentsDB.AddItemAsync(assignment);
+            await ExecuteLoadArchive();
+        }
+        private async void ClearArchive()
+        {
+            var deletedAssignments = (await App.AssignmentsDB.GetItemsAsync()).Where(t => t.IsDeleted == true);
+            foreach (var item in deletedAssignments)
+            {
+                await App.AssignmentsDB.DeleteItemAsync(item.ID);
+            }
+        }
+    }
+}
--- App1/ViewModels/SearchPageViewModel.cs
+++ App1/ViewModels/SearchPageViewModel.cs
@@ -16,55 +16,26 @@ namespace App1.ViewModels
     {
         public Command SearchBarTextChangedCommand { get; }
         private List<AssignmentModel> searchedAssignments = new List<AssignmentModel>();
-        public Command ClearArchiveCommand { get; }
-        public Command LoadArchiveCommand { get; }
         public List<AssignmentModel> SearchedAssignments { get => searchedAssignments; set { searchedAssignments = value; OnPropertyChanged("SearchedAssignments"); } }
 
-        public ObservableCollection<AssignmentModel> Archive { get; set; }
         public SearchPageViewModel()
         {
-            SearchBarTextChangedCommand = new Command<object>(OnSearchBarTextChanged);
-            ClearArchiveCommand = new Command(ClearArchive);
-            LoadArchiveCommand = new Command(async () => await ExecuteLoadArchive());
-            Archive = new ObservableCollection<AssignmentModel>();
+            //SearchBarTextChangedCommand = new Command<object>(OnSearchBarTextChanged);
         }
 
-        async Task ExecuteLoadArchive()
-        {
-            try
-            {
-                Archive.Clear();
-                var assList = (await App.AssignmentsDB.GetItemsAsync()).Where(t => t.IsDeleted == true);///GetSortedByDate(DateTime date);
-                foreach (var ass in assList)
-                {
-                    Archive.Add(ass);
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-        private async void ClearArchive()
-        {
-            var deletedAssignments = (await App.AssignmentsDB.GetItemsAsync()).Where(t => t.IsDeleted == true);
-            foreach (var item in deletedAssignments)
-            {
-                await App.AssignmentsDB.DeleteItemAsync(item.ID);
-            }
-        }
-        private async void OnSearchBarTextChanged(object obj)
-        {
-            if (obj is TextChangedEventArgs args)
-            {
+       
+        //private async void OnSearchBarTextChanged(object obj)
+        //{
+        //    if (obj is TextChangedEventArgs args)
+        //    {
                 
-                string filter = args.NewTextValue;
-                var archives = (await App.AssignmentsDB.GetItemsAsync()).Where(x => x.Name.ToLower().Contains(filter.Trim().ToLower()));
-                foreach(var item in archives)
-                {
-                    Archive.Add(item);
-                }
-            }
-        }
+        //        string filter = args.NewTextValue;
+        //        var archives = (await App.AssignmentsDB.GetItemsAsync()).Where(x => x.Name.ToLower().Contains(filter.Trim().ToLower()));
+        //        foreach(var item in archives)
+        //        {
+        //            Archive.Add(item);
+        //        }
+        //    }
+        //}
     }
 }
