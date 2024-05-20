422e6fae16efa1b1f0237dc1adddfd22dddcc23a
DaddyMilkere <Necessless@DESKTOP-DF5DPU5>
2024-05-21 00:25:59 +0500

    то же саоме

--- App1/ViewModels/Notification1PopupViewModel.cs
+++ App1/ViewModels/Notification1PopupViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace App1.ViewModels
 {
@@ -29,37 +30,12 @@ namespace App1.ViewModels
 
         public Notification1PopupViewModel(INavigation navigation)
         {
-            notificationManager = DependencyService.Get<INotificationManager>();
             Navigation = navigation;
-            DateSelectedCommand = new Command<string>(DateChanged);
+            DateSelectedCommand = new Command(DateChanged);
         }
-        
-        private async void DateChanged(string time)
+       
+        private async void DateChanged()
         {
-            string title = $"Уведомление!";
-            string message = $"Ваш дедлайн приближается!";
-            switch (time)
-            {
-                case "Нет":
-                    break;
-                case "Во время":
-                    notificationManager.SendNotification(title, message, SelectedDate);
-                    break;
-                case "За 5 минут":
-                    notificationManager.SendNotification(title, message, SelectedDate.AddMinutes(-5));
-                    break;
-                case "За 30 минут":
-                    notificationManager.SendNotification(title, message, SelectedDate.AddMinutes(-30));
-                    break;
-                case "За 1 час":
-                    notificationManager.SendNotification(title, message, SelectedDate.AddHours(-1));
-                    break;
-                case "За 1 день":
-                    notificationManager.SendNotification(title, message, SelectedDate.AddDays(-1));
-                    break;
-                case "Произвольно":
-                    break;
-            }
             await Navigation.PopPopupAsync();
         }
     }
--- App1/Views/Popups/Notification1PopupPage.xaml.cs
+++ App1/Views/Popups/Notification1PopupPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using App1.ViewModels;
+﻿using App1.Services.Notifications;
+using App1.ViewModels;
 using Rg.Plugins.Popup.Pages;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,37 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace App1.Views.Popups
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Notification1PopupPage : PopupPage
     {
+        INotificationManager notificationManager;
+        private DateTime _selectedDate;
+        public DateTime SelectedDate
+        {
+            get { return _selectedDate; }
+            set
+            {
+                if (_selectedDate != value)
+                {
+                    _selectedDate = value;
+                    OnPropertyChanged(nameof(SelectedDate));
+                }
+            }
+        }
         public Notification1PopupPage()
         {
             InitializeComponent();
             BindingContext = new Notification1PopupViewModel(Navigation);
+            notificationManager = DependencyService.Get<INotificationManager>();
+            notificationManager.NotificationReceived += (sender, eventArgs) =>
+            {
+                var evtData = (NotificationEventArgs)eventArgs;
+                ShowNotification(evtData.Title, evtData.Message);
+            };
         }
         public Notification1PopupPage(DateTime dateTime)
         {
@@ -25,8 +47,67 @@ namespace App1.Views.Popups
             BindingContext = new Notification1PopupViewModel(Navigation);
             if (dateTime != null)
             {
+                SelectedDate = dateTime;
                 (BindingContext as Notification1PopupViewModel).SelectedDate=dateTime;
             }
+            notificationManager = DependencyService.Get<INotificationManager>();
+            notificationManager.NotificationReceived += (sender, eventArgs) =>
+            {
+                var evtData = (NotificationEventArgs)eventArgs;
+                ShowNotification(evtData.Title, evtData.Message);
+            };
+        }
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            
+            string title = $"Уведомление!";
+            string message = $"Ваш дедлайн приближается!";
+            notificationManager.SendNotification(title, message, SelectedDate);
+        }
+        private void Button3_Clicked(object sender, EventArgs e)
+        {
+            string title = $"Уведомление!";
+            string message = $"Ваш дедлайн приближается!";
+            notificationManager.SendNotification(title, message, SelectedDate.AddMinutes(-5));
+        }
+        private void Button4_Clicked(object sender, EventArgs e)
+        {
+            string title = $"Уведомление!";
+            string message = $"Ваш дедлайн приближается!";
+            notificationManager.SendNotification(title, message, SelectedDate.AddMinutes(-30));
+        }
+        private void Button5_Clicked(object sender, EventArgs e)
+        {
+            string title = $"Уведомление!";
+            string message = $"Ваш дедлайн приближается!";
+            notificationManager.SendNotification(title, message, SelectedDate.AddHours(-1));
+        }
+        private void Button6_Clicked(object sender, EventArgs e)
+        {
+            string title = $"Уведомление!";
+            string message = $"Ваш дедлайн приближается!";
+            notificationManager.SendNotification(title, message, SelectedDate.AddDays(-1));
+        }
+        private void Button7_Clicked(object sender, EventArgs e)
+        {
+            string title = $"Уведомление!";
+            string message = $"Ваш дедлайн приближается!";
+            notificationManager.SendNotification(title, message, DateTime.Now.AddSeconds(15));
+        }
+        void ShowNotification(string title, string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Label()
+                {
+                    Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+                };
+                Layouter.Children.Add(msg);
+            });
         }
     }
 }
