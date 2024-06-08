93b56ced0123916b289941b302bbb9d10a273bc0
DaddyMilkere <Necessless@DESKTOP-DF5DPU5>
2024-06-09 01:04:24 +0500

    фывы

--- App1/Models/ListModel.cs
+++ App1/Models/ListModel.cs
@@ -1,21 +1,40 @@
 ﻿
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using SQLite;
 
 namespace App1.Models
 {
     public class ListModel : BaseModel
     {
+        private int _count;
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string Name { get; set; }
-        public int Count { get; set; }
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
         public string Icon { get; set; }
         public string Color { get;set; }
 
-        public async void UpdateCount()
+        public async Task UpdateCount()
         {
-            Count = ((await App.AssignmentsDB.GetItemsAsync()).Where(x => x.FolderName == Name).Count());
+            var items = await App.AssignmentsDB.GetItemsAsync();
+            Count = items.Count(x => x.FolderName == Name);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
